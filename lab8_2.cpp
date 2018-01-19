d593dd329cb09913bf5b149a5db5b965042e27c8
Unknown <unknown@example.com>
2018-01-19 14:26:36 +0700

    ทำข้อ2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -7,11 +7,23 @@ int main () {
         char name[] = {"Anukul"};
         int nameLength = 6;
         cout << name;
-
+        char nom[7]
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
-
-
-
+        nom 0 = name [6]
+        nom 1 = name [5]
+        nom 2 = name [4]
+        nom 3 = name [3]
+        nom 4 = name [2]
+        nom 5 = name [1]
+        nom 6 = '\0';
+        cout nom <<endl;
+        nom 0 = nom [1] + 1;
+        nom 1 = nom [1] + 1;
+        nom 2 = nom [2] + 1;
+        nom 3 = nom [3] + 1;
+        nom 4 = nom [4] + 1;
+        nom 5 = nom [5] + 1;
+        cout nom <<endl;
         return 0;
 }
